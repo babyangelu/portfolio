068d481f33dd739f32517a2147f07af00273966b
‘babyangelu <babyangelu@gmail.com>
2026-01-16 14:48:03 +0900

    fix: path

---
+++ src/main/java/com/example/portfolio/RootController.java
@@ -0,0 +1,13 @@
+package com.example.portfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "forward:/portfolio/index.html";
+    }
+}
